0eaaf954c74097f5e51e8b398ab889abc089bbb8
Luis <luchor13@gmail.com>
2019-05-30 12:36:25 -0500

    correccion rest agregado

--- src/main/java/pe/com/unmsm/sgojp/api/controller/Impl/Test.java
+++ src/main/java/pe/com/unmsm/sgojp/api/controller/Impl/Test.java
@@ -5,9 +5,19 @@
  */
 package pe.com.unmsm.sgojp.api.controller.Impl;
 
+import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import pe.com.unmsm.sgojp.api.dao.ActivityDAO;
+import pe.com.unmsm.sgojp.api.dao.impl.ActivityDAOImpl;
+import pe.com.unmsm.sgojp.api.model.Activity;
+import pe.com.unmsm.sgojp.api.model.User;
 
 /**
  *
@@ -16,10 +26,34 @@ import javax.ws.rs.PathParam;
 @Path("/test")
 public class Test {
     
+    static ActivityDAO a = new ActivityDAOImpl();
+
+    private static List<Activity> ls = a.getAll();
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getActivityes() {
+        System.out.println("GET Activity");
+        return Response.ok(ls).build();
+    }
+    
     @GET
     @Path("/{file}")
     public String prueba(@PathParam("file") final String file){
         return file;
     }
-    
+//    @POST
+//    @Path("/post")
+//    public User pruebaPost(@RequestBody User user){
+//        return user;
+//    }
+    @POST
+    @Path("/post/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response registerActivity(Activity activity) {
+        System.out.println("POST Activity");
+        System.out.println(activity);
+        return Response.status(Response.Status.CREATED).entity(activity).build();
+    }
 }
